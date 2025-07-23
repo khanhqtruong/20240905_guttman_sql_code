23a128cda615407727711b13c3dc5de2b1ceb1f6
khanhqtruong <kqtnet@gmail.com>
2025-07-23 13:49:12 -0400

    Update project03_test.sql

--- project03_test.sql
+++ project03_test.sql
@@ -8,3 +8,6 @@ SELECT
         product;
 
         select 'Successful' AS 'Installation', product_id, product_name, product_price, category_id from product;
+
+
+-- This is just a comment added today 7.23.2025
